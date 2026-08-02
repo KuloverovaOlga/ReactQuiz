870d591007c3b5cd2d2b681fbbc72d2f670d869b
O.Kuloverova <o.kuloverova@chekiizapteki.ru>
2026-08-03 03:07:11 +0400

    add server

--- src/pages/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as HomePage } from './HomePage/HomePage';
-export { default as CatalogPage } from './CatalogPage/CatalogPage';
-export { default as QuizePage } from './QuizePage/QuizePage';
